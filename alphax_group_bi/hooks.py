f7e69d3cd00b43ec0f7480b88657b62fac228a4d
Nooruddin Shaik <noor@neotec.ai>
2026-03-12 15:19:05 +0300

    Fix Frappe app import naming

--- alphax_group_bi/hooks.py
+++ alphax_group_bi/hooks.py
@@ -1,12 +1,6 @@
 app_name = "alphax_group_bi"
 app_title = "AlphaX Group BI"
-app_publisher = "Neotec Integrated Solutions"
-app_description = "Financial matrix builder for ERPNext / Frappe"
-app_email = "support@example.com"
+app_publisher = "Neotec"
+app_description = "Financial Matrix Builder for ERPNext"
+app_email = "support@neotec.com"
 app_license = "MIT"
-
-app_include_css = ["/assets/alphax_group_bi/css/group_bi.css"]
-
-doctype_js = {
-    "AGB Template": "public/js/agb_template.js",
-}
--- setup.py
+++ setup.py
@@ -1,18 +1,13 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as f:
-    long_description = f.read()
-
 setup(
     name="alphax_group_bi",
-    version="1.1.0",
-    description="Financial matrix builder for Frappe / ERPNext",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    author="OpenAI for Nooruddin Shaik",
-    author_email="support@example.com",
+    version="1.1.1",
+    description="Financial Matrix Builder for ERPNext",
+    author="Neotec",
+    author_email="support@neotec.com",
     packages=find_packages(),
-    include_package_data=True,
     zip_safe=False,
+    include_package_data=True,
     install_requires=[],
 )
